8e6c5a2c21b85e261a5ab37c055423bf8b6544e6
Mario Thorpe <mariothorpe22@gmail.com>
2025-04-03 03:03:47 -0400

    Chore: reformatted return statement for better readability in TodoList

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -11,7 +11,7 @@ function TodoList() {
         <ul>
             {todos.map((todo) => ( 
                 <TodoListItem key={todo.id} todo={todo} />
-          ))}
+            ))}
         </ul>
     );
 }
