69a98668a7dc7ec88292a3d9bae46344b9fbcc09
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-04-13 19:54:14 -0400

    added handlers and updated state todolist.jsx app.jsx todoform.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,19 +1,13 @@
-import TodoListItem from "./TodoListItem";
+import TodoListItem from './TodoListItem';
 
-function TodoList() {
-    const todos = [
-        { id: 1, title: "Review Resources"},
-        { id: 2, title: "Take Notes"},
-        { id: 3, title: "Code Out App"},
-      ];
-
-    return (
-        <ul>
-            {todos.map((todo) => ( 
-                <TodoListItem key={todo.id} todo={todo} />
-            ))}
-        </ul>
-    );
+function TodoList({ todoList }) {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
