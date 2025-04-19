93bc500d80b60ce8437bc015b0191396dc3b45bc
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-04-18 23:40:51 -0400

    week-05 feat: complete todos, disabled AddTodo button, controlled form

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,19 +1,29 @@
 import { useRef } from 'react';
+import { useState } from 'react';
 
 function TodoForm({ onAddTodo }) {
+  const [workingTodo, setWorkingTodo] = useState('');
   const todoTitleInput = useRef('');
   function handleAddTodo(event) {
     event.preventDefault();
-    const title = event.target.title.value;
-    onAddTodo(title);
-    event.target.title.value = '';
+    onAddTodo(workingTodo);
+    setWorkingTodo('');
     todoTitleInput.current.focus();
   }
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle"> To Do </label>
-      <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
-      <button type="submit"> Add Todo </button>
+      <input
+        type="text"
+        id="todoTitle"
+        name="title"
+        ref={todoTitleInput}
+        value={workingTodo}
+        onChange={(e) => setWorkingTodo(e.target.value)}
+      />
+      <button type="submit" disabled={workingTodo === ''}>
+        Add Todo
+      </button>
     </form>
   );
 }
