08f178e26f9614da12b9c9f687a41c372a4e07a4
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-04-19 16:00:59 -0400

    feat: added editing functionality to Todo List.
    - Todo item can be marked as completed.
    - Todo item title can now be edited.
    - Created cancel and update buttons.
    - Reorganize project, added features and shared directory.
    - Refactored input and label into a reusable component named TextInputWithLabel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import TodoList from './features/TodoList';
+import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 import { useState } from 'react';
 
@@ -9,6 +9,7 @@ function App() {
     const newTodo = { title: title, id: Date.now(), isCompleted: false };
     setTodoList([...todoList, newTodo]);
   }
+
   function completeTodo(id) {
     const updatedTodos = todoList.map((todo) => {
       if (todo.id === id) {
@@ -18,11 +19,26 @@ function App() {
     });
     setTodoList(updatedTodos);
   }
+
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        return { ...editedTodo };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
+  }
+
   return (
     <div>
       <h1>My Todo List</h1>
       <TodoForm onAddTodo={handleAddTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
     </div>
   );
 }
